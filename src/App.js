f6c74dbe49d16fa68ebb9fd1034314fb30f36803
camille <spacejamilli20@gmail.com>
2021-02-18 00:23:32 -0600

    structured files/called API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+
+  const [newData, setNewData] = useState('');
+  const url = 'https://api.nasa.gov/planetary/apod?api_key=jHHaYDqZNaIIVFOBjzn6cq9bzRsNQ8JhlXlgDknX';
+
+  const getData = () => {
+    axios
+    .get(`${url}`)
+    .then((response) => {
+      setNewData(response.data);
+    })
+    .catch((error) => {
+      console.error(`Error: ${error}`)
+    });
+  }
+    
+    useEffect(() => {
+      getData();
+    }, []);
+    
+
+
   return (
     <div className="App">
       <p>
